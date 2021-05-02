36d847375c5ea3a2b1c438e13262cbe9507db0f3
victorex123 <lordshenvl@gmail.com>
2021-05-03 01:10:35 +0200

    MovimientoJugadorImplementado(BUG)
    Movimiento del jugador implementado parcialmente, actualmente está bug. Además la cámara requiere mejoras y un movimiento más suavizado.

--- AnimationProject/Assets/Scripts/Charactermovement/PlayerController.cs
+++ AnimationProject/Assets/Scripts/Charactermovement/PlayerController.cs
@@ -15,21 +15,32 @@ public class PlayerController : MonoBehaviour
     public float zoomSpeed = 150;
     public float rotationSpeed = 1.0f;
     public float aimSpeed = 50;
-    private float mouseX, mouseY;
+
+    public float movementSpeed = 5;
 
     //Private
 
     private float dt;
 
+    private Camera activeCamera;
+
     private Vector3 vectorDir = new Vector3();
     private Vector3 vectorDirLook = new Vector3();
+
+    private float mouseX, mouseY;
+
+
+    private void Awake()
+    {
+        activeCamera = playerAimCamera;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         mouseX = 0;
         mouseY = 0;
-        vectorDir = (targetCamera.transform.position - playerCamera.transform.position).normalized;
+        vectorDir = (targetCamera.transform.position - activeCamera.transform.position).normalized;
         vectorDirLook = (forwardPost.transform.position - transform.position).normalized;
         vectorDirLook.y = 0;
         Cursor.lockState = CursorLockMode.Locked;
@@ -41,13 +52,23 @@ public class PlayerController : MonoBehaviour
     {
         dt = Time.deltaTime;
 
-        vectorDir = (targetCamera.transform.position - playerCamera.transform.position).normalized;
+        vectorDir = (targetCamera.transform.position - activeCamera.transform.position).normalized;
 
         vectorDirLook = (forwardPost.transform.position - transform.position).normalized;
         vectorDirLook.y = 0;
 
         CameraMove();
         Look();
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            LookingForward();
+            transform.position += vectorDirLook * 1 * dt;
+        }
     }
     private void CameraMove()
     {
@@ -63,11 +84,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetMouseButton((1)))
         {
             CameraChange(false,true);
-            vectorDirLook = (forwardPost.transform.position - transform.position);
-            vectorDirLook.y = 0;
-            var rotation = Quaternion.LookRotation(vectorDirLook);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, dt * aimSpeed);
-            Debug.Log("Estoy funcionando");
+            LookingForward();
         }
         else
         {
@@ -75,10 +92,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void LookingForward()
+    {
+        vectorDirLook = (forwardPost.transform.position - transform.position);
+        vectorDirLook.y = 0;
+        var rotation = Quaternion.LookRotation(vectorDirLook);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, dt * aimSpeed);
+    }
+
     private void CameraChange(bool cam1, bool cam2)
     {
         playerCamera.gameObject.SetActive(cam1);
         playerAimCamera.gameObject.SetActive(cam2);
+
+        if (activeCamera == playerCamera)
+        {
+            activeCamera = playerAimCamera;
+        }
+        else
+        {
+            activeCamera = playerCamera;
+        }
 
     }
 }
