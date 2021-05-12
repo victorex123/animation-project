7ff0a54a0d0ad6cac0442738580ae97a6efbdc1f
VicMan12 <victormanuel.a.lopez@gmail.com>
2021-05-12 18:09:30 +0200

    some update lobby

--- AnimationProject/Assets/Scripts/LorePagesManager.cs
+++ AnimationProject/Assets/Scripts/LorePagesManager.cs
@@ -11,34 +11,32 @@ public class LorePagesManager : MonoBehaviour
     Queue<Image> enemyImages;
 
     public GameObject controlPanel;
-    public GameObject EnemiesPanel;
-    public GameObject ObjectivePanel;
-    public GameObject ButtonPanel;
+    public GameObject enemiesPanel;
+    public GameObject objectivePanel;
+    public GameObject buttonPanel;
+    public GameObject principalLoreCanvas;
     public Text displayText;
     public Image displayImage;
 
     string activeSentence;
     Image activeImage;
+    private GameObject actualCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        enemySentences = new Queue<string>();
+        enemyImages = new Queue<Image>();
     }
 
     public void BackButtonsPanel()
     {
-
+        actualCanvas.SetActive(false);
+        buttonPanel.SetActive(true);
     }
 
     public void CloseLorePages()
     {
-
+        principalLoreCanvas.SetActive(false);
     }
 
     public void GoToRightPage()
@@ -49,6 +47,42 @@ public class LorePagesManager : MonoBehaviour
     public void GoToLeftPage()
     {
 
+    }
+
+    public void EnemiesPage()
+    {
+        buttonPanel.SetActive(false);
+        enemiesPanel.SetActive(true);
+        actualCanvas = enemiesPanel;
+    }
+
+    public void ControlPage()
+    {
+        buttonPanel.SetActive(false);
+        controlPanel.SetActive(true);
+        actualCanvas = controlPanel;
+    }
+
+    public void ObjetivePage()
+    {
+        buttonPanel.SetActive(false);
+        objectivePanel.SetActive(true);
+        actualCanvas = objectivePanel;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if ((other.CompareTag("Player")) && Input.GetKeyDown(KeyCode.Q))
+        {
+            principalLoreCanvas.SetActive(true);
+}
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            principalLoreCanvas.SetActive(false);
+        }
     }
 
 }
