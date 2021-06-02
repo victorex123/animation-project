7499266254af3c7773885a926cabad2d46deb8de
victorex123 <lordshenvl@gmail.com>
2021-06-02 17:17:00 +0200

    Subida

---
+++ AnimationProject/Assets/Scripts/SceneEvents/FinalSceneEvent.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalSceneEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Public
+
+
+
+    //Private
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
